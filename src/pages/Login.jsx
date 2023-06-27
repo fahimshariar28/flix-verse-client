6d174f1e28daf8b59bf5d0cfad5f5ed95926504a
Fahim Shariar <fahimshariar28@gmail.com>
2023-06-28 02:40:58 +0600

    Login system emplement

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,73 @@
+import { Link, useNavigate } from "react-router-dom";
+import BackgroundImage from "../components/BackgroundImage";
+import Header from "../components/Header";
+import { useContext } from "react";
+import { AuthContext } from "../provider/AuthProvider";
+
 const Login = () => {
-  return <div>Login</div>;
+  const { loginUser } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+    loginUser(email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        navigate("/");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+        // ..
+      });
+  };
+
+  return (
+    <div className="relative">
+      <BackgroundImage />
+      <div className="absolute top-0 left-0 bg-black-rgba h-screen w-screen">
+        <Header login />
+        <div className="mt-5 text-white flex justify-center items-center flex-col">
+          <h2 className="text-3xl font-bold">Login</h2>
+          <form onSubmit={handleLogin}>
+            <input
+              className="text-black border-0 py-2 px-4 rounded-sm mt-3 text-lg block"
+              type="email"
+              name="email"
+              placeholder="Enter Your Email"
+            />
+            <input
+              className="text-black border-0 py-2 px-4 rounded-sm mt-3 text-lg block"
+              type="password"
+              name="password"
+              placeholder="********"
+            />
+            <input
+              type="submit"
+              value="Login"
+              className="px-4 py-2 bg-red-600 border-0 text-white rounded font-bold text-base cursor-pointer block mt-3"
+            />
+          </form>
+          <p className="mt-5">
+            Don&lsquo;t have an account{" "}
+            <Link
+              className="px-4 py-2 bg-red-600 border-0 text-white rounded font-bold text-base"
+              to="/signup"
+            >
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
