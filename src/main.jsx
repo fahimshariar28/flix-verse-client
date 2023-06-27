c852cbd17ecad6dd03fc122ef4d0129612d14cda
Fahim Shariar <fahimshariar28@gmail.com>
2023-06-28 03:06:06 +0600

    private route set

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,16 @@ import FlixVerse from "./pages/FlixVerse";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import AuthProvider from "./provider/AuthProvider";
+import PrivateRoute from "./routes/PrivateRoute";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <FlixVerse />,
+    element: (
+      <PrivateRoute>
+        <FlixVerse />
+      </PrivateRoute>
+    ),
   },
   {
     path: "/login",
